e3099eee2d5ed106d56656b8a913fabd10ff2bee
Lakmina Chathurya <36987117+Chathurya1994@users.noreply.github.com>
2020-03-23 12:47:48 +0530

    Add files via upload

---
+++ Abc.java
@@ -0,0 +1,56 @@
+package com.virtu.gt.ins;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+/**
+ * Created by LAWEERASINGHE on 2/1/2018.
+ */
+public class Abc {
+
+    public Abc() {
+
+    }
+
+    //Comment
+    //Comment
+
+    private String s;
+    private String d;
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+
+    public static void main(String[] args) {
+        List<String> names = Lists.newArrayList();
+        names.add("Lasitha");
+        names.add("Nuwan");
+    }
+
+
+    private void testPrivateMethod() {
+        System.out.println("test private method");
+    }
+
+
+}
+class SampleClass_1550722670790_13271000_ {
+}
+class SampleClass_1550722692385_3371000_ {
+}
+class SampleClass_1550722713242_20291000_ {
+}
+class SampleClass_1550722731837_7271000_ {
+}
+class SampleClass_1550722751670_23971000_ {
+}
+class SampleClass_1550722777485_1161000_ {
+}
+class SampleClass_1550723595243_22941000_ {
+}
